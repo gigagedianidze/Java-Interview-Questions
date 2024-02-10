faa8efd0f18879e7bd2032e237ebdd34acc35b0b
user1 <gigagedianidze@gmail.com>
2024-02-10 08:09:20 -0500

    count sum of the digits in the number included in this commit.

--- src/main/java/CountSumOfDigitsInANumber/CountSumOfDigitsInANumber.java
+++ src/main/java/CountSumOfDigitsInANumber/CountSumOfDigitsInANumber.java
@@ -2,6 +2,16 @@ package CountSumOfDigitsInANumber;
 
 public class CountSumOfDigitsInANumber {
     public static void main(String[] args){
-        System.out.println("kai kacis branchi");
+
+        int num = 2549;
+        int sum = 0;
+
+        while (num>0)
+        {
+            sum = sum + num%10;
+            num = num/10;
+        }
+
+        System.out.println("Sum of the digits in number is " + sum );
     }
 }
