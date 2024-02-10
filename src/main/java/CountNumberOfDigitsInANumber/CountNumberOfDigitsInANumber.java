ff59c53e9540b713164edafd26677ba7b461cfda
user1 <gigagedianidze@gmail.com>
2024-02-09 21:44:19 -0500

    third commit

--- src/main/java/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber.java
+++ src/main/java/CountNumberOfDigitsInANumber/CountNumberOfDigitsInANumber.java
@@ -1,4 +1,16 @@
 package CountNumberOfDigitsInANumber;
 
 public class CountNumberOfDigitsInANumber {
+    public static void main(String[] args) {
+
+        int num = 7123456;
+        int  count = 0;
+
+        while(num>0)
+        {
+            num = num/10;
+            count++;
+        }
+        System.out.println("Number of digits " +count);
+    }
 }
